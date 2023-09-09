2e35217a0af9764da51353142e36ed9a2e2d76ac
Perxitas <129907605+Perxitas@users.noreply.github.com>
2023-09-09 19:56:14 -0300

    Add files via upload

---
+++ Tarea2/Bomba.c
@@ -0,0 +1,125 @@
+#include "Bomba.h"
+#include "Tierra.h"
+#include "Tablero.h"
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+extern int dimension;
+extern char** MatrizBTD;
+
+/*TryExplotar()
+/******/
+/*int fila: recibe un entero de la fila en la cual se hara la funcion
+/*int columna: recibe un entero de la columna en la cual se hara la funcion
+/******/
+/*
+Se declara Bomb, para facilitar el manejo de la bomba deseada, luego a travez de condicionales se verifica el tipo de bomba para entrar a la funcion
+de su tipo, si es que no estan aun listas para explotar, se les reduce la vida.
+*/
+
+void TryExplotar(int fila, int columna){
+    Bomba* Bomb=(Bomba*)tablero[fila][columna];
+    if(Bomb->explotar==ExplosionPunto && Bomb->contador_turnos == 0){
+        ExplosionPunto(fila,columna);
+    }else if(Bomb->explotar== ExplosionX && Bomb->contador_turnos == 0){
+        ExplosionX(fila,columna);   
+    }else{
+        Bomb->contador_turnos -= 1;
+    }
+    return;
+}
+
+/*BorrarBomba()
+/******/
+/*int fila: recibe un entero de la fila en la cual se hara la funcion
+/*int columna: recibe un entero de la columna en la cual se hara la funcion
+/******/
+/*
+Gracias a la declaracion de bomb, se devuelve la tierra donde alguna vez estuvo, sacandola del struct de la bomba, para luego verificar a travez de
+condicionales y la MatrizBTD, que si es que hubo tesoro ahi, que la MatrizBTD vuelva a 'T' de lo contrario que vuelva a ' '
+*/
+
+void BorrarBomba(int fila, int columna){    
+    Bomba* Bomb = (Bomba*)tablero[fila][columna];
+    tablero[fila][columna] = Bomb->tierra_debajo;
+    if(MatrizBTD[fila][columna]=='D'){
+        MatrizBTD[fila][columna]='T';
+    }else{
+        MatrizBTD[fila][columna]=' ';
+    }
+
+    free(Bomb);
+    return;
+}
+
+/*ExplosionPunto()
+/******/
+/*int fila: recibe un entero de la fila en la cual se hara la funcion
+/*int columna: recibe un entero de la columna en la cual se hara la funcion
+/*
+A travez de Bomb, se reduce la vida de la tierra, siendo este daño de 3 la vida queda en 0, luego llama a la funcion BorrarBomba
+*/
+
+void ExplosionPunto(int fila, int columna){
+    Bomba* Bomb =(Bomba*)tablero[fila][columna];
+    Bomb->tierra_debajo->vida = 0;
+    BorrarBomba(fila,columna);
+    return;
+}
+
+/*ExplosionX()
+/******/
+/*int fila: recibe un entero de la fila en la cual se hara la funcion
+/*int columna: recibe un entero de la columna en la cual se hara la funcion
+/******/
+/*
+Se declaran variables enteros, para luego restarle la vida a la posicion donde esta la bomba, luego a travez de una matriz para obtener las coordenadas 
+en forma de X, entrando al bucle, generamos las nuevas direcciones, hasta 4 veces para poder generar la X, si las coordenadas se salen de la matriz se
+ajustan a las dimensiones de la matriz, luego de esto, se declara tierraX, que sera las diagonales que se les quitara vida, terminando el for
+verificamos que tipo era con MatrizBTD, para concluir con BorrarBomba()
+*/
+
+void ExplosionX(int fila, int columna){
+    int filaX,columnaX;
+
+    ((Bomba*)tablero[fila][columna])->tierra_debajo->vida -= 1;
+    
+    int formaX[4][2] = {
+        {-1, -1}, 
+        {-1, 1}, 
+        {1, -1}, 
+        {1, 1}
+    };
+
+    for (int i = 0; i < 4; i++) {
+        filaX = fila + formaX[i][0];
+        columnaX = columna + formaX[i][1];
+
+        if (filaX < 0) {
+            filaX = dimension - 1;
+        } else if (filaX >= dimension) {
+            filaX = 0; 
+        }
+        if (columnaX < 0) {
+            columnaX = dimension - 1; 
+        } else if (columnaX >= dimension) {
+            columnaX = 0; 
+        }
+
+        Tierra* tierraX = (Tierra*)tablero[filaX][columnaX];
+        tierraX->vida -=1;
+        if(tierraX->vida<=0){
+            tierraX->vida=0;
+        }
+        
+    }
+    if(MatrizBTD[fila][columna]=='D'){
+        MatrizBTD[fila][columna]='T';
+    }else{
+        MatrizBTD[fila][columna]=' ';
+    }
+    BorrarBomba(fila,columna);
+    return;
+}
+ 
---
+++ Tarea2/TreasureFinder.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include<stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include "Tierra.h"
+#include "Tablero.h"
+#include "Bomba.h"
+
+/*main()
+Declarando varios enteros y asignando memoria a la variable Bomb, entra a un while, el cual detecta que la CantTesoros siempre este arriba de 0,
+empezando por una bienvenida si es que estamos en el primer turno, preguntamos el tamaño de la tabla, luego verifica que la accion sea 1 (evita repetir
+la matriz si es que se selecciono 2 o 3),luego si la accion es 2 o 3 para sus respectivas funciones,si no se sigue pidiendo informacion y asi llamar 
+finalmente a la funcion ColocarBomba, incrementa los Turnos y se vuelve al loop.
+Al salir del while, se gana el juego al haber encontrado todos los tesoros(o se gana instantaneamente si no se genero ninguno).
+*/
+int main(){
+    srand(time(0));
+
+    int accion=1 ,X ,Y ,FormaExpl ,Tamaño ,n ,NumTurno = 1;
+    
+    Bomba* Bomb = (Bomba*)malloc(sizeof(Bomba));
+
+    while (CantTesoros > 0 || NumTurno==1){
+        if (NumTurno == 1){
+            printf("Bienvenido a TreasureFinder!\n\nPorfavor, Indicar el tamaño del tablero a jugar:\n\n1.7x7 2.10x10 3.12x12\n\nSu Elección: ");
+            scanf("%d",&n);
+            if(n==1){
+                Tamaño=7;   
+            }else if(n==2){
+                Tamaño=10;    
+            }
+            else if(n==3){
+                Tamaño=12;    
+            }
+            IniciarTablero(n);
+            printf("Comenzando el juego...\n\n");
+        }
+
+        if(accion != 3 && accion != 2){ //Para no repetir la matriz mas de 1 vez
+            printf("Tablero (Turno %d)\n", NumTurno);
+            MostrarTablero();
+        }
+        
+        printf("\nSeleccione una accion: \n1.Colocar Bomba 2.Mostrar Bombas 3.Mostrar Tesoros\nEscoja una opcion: ");
+        scanf("%d",&accion);
+        printf("\n\n");
+
+        if(accion==1){ 
+            printf("Indique coordenadas de la bomba:\n\nFila:");
+            scanf("%d",&X);
+            printf("\n");
+            printf("Columna:");
+            scanf("%d",&Y);
+            printf("\n");
+            printf("Indique la forma en que explota la bomba:\n\n1.Punto 2.X\n\nSu input:");
+            scanf("%d",&FormaExpl);
+            printf("\n");
+            if(FormaExpl==1){
+                Bomb->explotar = ExplosionPunto; 
+            }else if(FormaExpl==2){
+                Bomb->explotar=ExplosionX;   
+            }
+            ColocarBomba(Bomb, X, Y);
+            PasarTurno();
+        }else if(accion==2){
+            MostrarBombas();
+        }else if(accion==3){
+            VerTesoros();
+        }
+        NumTurno++;
+    }   
+   
+    printf("Ha encontrado todos los tesoros y por ende a ganado!\n\nTabla Final:\n\n");
+    MostrarTablero();
+            
+    BorrarTablero();
+    free(Bomb);
+}
